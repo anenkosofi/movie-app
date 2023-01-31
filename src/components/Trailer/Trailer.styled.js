0049cffbf8675d8c50b7480e7821d2dc66e9863c
sofidorosh <108129293+anenkosofi@users.noreply.github.com>
2023-01-31 18:31:47 +0200

    Edit Trailer container

--- src/components/Trailer/Trailer.styled.js
+++ src/components/Trailer/Trailer.styled.js
@@ -1,5 +1,26 @@
 import styled from 'styled-components';
 
 export const TrailerWrapper = styled.div`
-  text-align: center;
+  width: 280px;
+  height: 200px;
+  margin: 0 auto;
+
+  @media screen and (min-width: 480px) {
+    width: 320px;
+    height: 240px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 720px;
+    height: 405px;
+  }
+  @media screen and (min-width: 1280px) {
+    width: 848px;
+    height: 477px;
+  }
+
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
 `;
